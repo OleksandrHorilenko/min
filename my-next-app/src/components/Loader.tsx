e46c75fa53d682e6d482201d150aa4f761b145eb
Oleksandr Horilenko <a.gorilenko82@icloud.com>
2024-12-02 03:01:35 +0200

    fix loader

--- my-next-app/src/components/Loader.tsx
+++ my-next-app/src/components/Loader.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { CircleLoader } from 'react-spinners';
+//import { CircleLoader } from 'react-spinners';
 
 const Loader = () => {
   return (
     <div className="loader-container">
-      <CircleLoader color="#3498db" size={50} />
+      
       <div className="loader-text">loading...</div>
     </div>
   );
