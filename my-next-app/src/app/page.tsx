86ba8921d6dc496228b62a25749a7a8f2cb8111d
Oleksandr Horilenko <a.gorilenko82@icloud.com>
2024-12-02 04:57:41 +0200

    vercel fix

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -6,7 +6,7 @@
  * YouTube: https://www.youtube.com/@NikandrSurkov
  * GitHub: https://github.com/nikandr-surkov
  */
-'use client' 
+'use client'
 
 import CheckFootprint from '@/components/CheckFootprint'
 import NavigationBar from '@/components/NavigationBar'
@@ -26,33 +26,35 @@ interface UserData {
 }
 
 export default function Home() {
-
   const [userData, setUserData] = useState<UserData | null>(null)
-  
+
   useEffect(() => {
-    if (WebApp.initDataUnsafe?.user) {
-      const user = WebApp.initDataUnsafe.user as UserData;
-      setUserData(user);
-      localStorage.setItem('userData', JSON.stringify(user)); // Сохраняем данные в localStorage
-    } else {
-      // Если пользователь не из Telegram, ставим тестовые данные
-      const testUserData = {
-        id: 12345,
-        first_name: 'Test User',
-        last_name: 'Testov',
-        username: 'testuser',
-        language_code: 'en',
-        is_premium: true,
-      };
-      setUserData(testUserData);
-      localStorage.setItem('userData', JSON.stringify(testUserData)); // Сохраняем тестовые данные
+    if (typeof window !== "undefined" && window.localStorage) {
+      // Проверяем, что код выполняется в браузере
+      if (WebApp.initDataUnsafe?.user) {
+        const user = WebApp.initDataUnsafe.user as UserData;
+        setUserData(user);
+        localStorage.setItem('userData', JSON.stringify(user)); // Сохраняем данные в localStorage
+      } else {
+        // Если пользователь не из Telegram, ставим тестовые данные
+        const testUserData = {
+          id: 12345,
+          first_name: 'Test User',
+          last_name: 'Testov',
+          username: 'testuser',
+          language_code: 'en',
+          is_premium: true,
+        };
+        setUserData(testUserData);
+        localStorage.setItem('userData', JSON.stringify(testUserData)); // Сохраняем тестовые данные
+      }
     }
   }, []);
 
   return (
     <TabProvider>
       <main className="min-h-screen bg-black text-white">
-        <CheckFootprint />
+        <CheckFootprint /> {/* Компонент CheckFootprint теперь не нуждается в пропсах */}
         <TabContainer />
         <NavigationBar />
       </main>
