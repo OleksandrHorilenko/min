ec7bca52f8337a8dfabf7dc2c28ce743c9119627
Oleksandr Horilenko <a.gorilenko82@icloud.com>
2024-12-05 04:25:16 +0200

    fix route

--- my-next-app/src/app/api/models/User.js
+++ my-next-app/src/app/api/models/User.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   TelegramId: { type: String, unique: true, required: true }, // Изменили на TelegramId
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  username: { type: String, required: true },
-  language_code: { type: String, required: true },
+  first_name: { type: String },
+  last_name: { type: String },
+  username: { type: String },
+  language_code: { type: String },
   is_premium: { type: Boolean, default: false },
 });
 
