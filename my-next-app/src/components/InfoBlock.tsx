7f7a83d6f89382e09d349aadac84845d82fff140
Oleksandr Horilenko <a.gorilenko82@icloud.com>
2025-01-02 11:08:44 +0200

    02012025 1108

--- my-next-app/src/components/InfoBlock.tsx
+++ my-next-app/src/components/InfoBlock.tsx
@@ -209,102 +209,126 @@ const InfoBlock: React.FC = () => {
 
   return (
     <div className="bg-[#121212] flex flex-col items-center">
-      <div className="w-full max-w-6xl bg-[#1a1a1a] rounded-2xl shadow-lg p-6 flex flex-col space-y-6">
-        <div className="text-white text-lg font-bold hover:text-gray-300 transition-colors flex flex-col items-center justify-center">
-          <span>Total mined since last claim:</span>
-          <span className="text-4xl">{getTotalMinedCoins().toFixed(3)}</span>
-        </div>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleClaim}
-          disabled={isButtonDisabled}
-          className={`bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold py-3 px-8 rounded-xl shadow-lg ${
-            isButtonDisabled ? "opacity-50 cursor-not-allowed" : "hover:from-blue-600 hover:to-purple-600 transition duration-300 ease-in-out transform hover:scale-105"
-          }`}
-        >
-          {isButtonDisabled ? `Wait ${Math.floor(countdown / 60)}:${String(countdown % 60).padStart(2, '0')}` : "Claim All"}
-        </Button>
-
-        {userCollection.length === 0 ? (
-  <div className="flex flex-col items-center justify-center w-full h-[200px] bg-gray-700 rounded-2xl shadow-lg p-6 mb-6">
-    <p className="text-xl text-white font-bold mb-4">You don't have any cards yet.</p>
-    <Button
-      variant="contained"
-      color="primary"
-      key={'leaderboard'}
-                                //onClick={() => setActiveTab('leaderboard')
-      onClick={() => console.log(setActiveTab('leaderboard'))}  // Логика для перехода к покупке
-      className="shine-effect bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold py-3 px-8 rounded-xl shadow-lg"
-
-    >
-      Buy your first card
-    </Button>
-  </div>
-) : (
-  userCollection.map((card, index) => {
-    const minedCoins = calculateMinedCoins(card, serverTime || new Date());
-    const remainingPercentage = ((card.remainingcoins / card.miningcoins) * 100).toFixed(2);
-
-    const bgColor = (() => {
-      switch (card.rarity) {
-        case "Common":
-          return "bg-[#121212] border-[#8A2BE2]"; // Темный фон с фиолетовым обрамлением
-        case "Rare":
-          return "bg-[#1B0036] border-[#00FFFF]"; // Темно-фиолетовый фон с неоновым голубым
-        case "Epic":
-          return "bg-[#30003B] border-[#FF00FF]"; // Глубокий пурпурный фон с ярким розовым
-        default:
-          return "bg-[#1A1A1A] border-[#C0C0C0]"; // Серый фон с светлым металлическим контуром
-      }
-    })();
-
-    return (
-      <div
-        key={index}
-        className={`relative w-[320px] h-[200px] ${bgColor} border-2 rounded-2xl shadow-lg p-6 flex flex-col justify-between mb-6 mx-auto`}
-        style={{
-          backgroundImage: 'radial-gradient(circle, rgba(255,255,255,0.1) 0%, transparent 60%)',
-        }}
+    <div className="w-full max-w-6xl bg-[#1a1a1a] rounded-2xl shadow-lg p-6 flex flex-col space-y-6 items-center justify-center">
+      <div className="text-white text-lg font-bold hover:text-gray-300 transition-colors flex flex-col items-center justify-center">
+        <span>Total mined since last claim:</span>
+        <span className="text-4xl">{getTotalMinedCoins().toFixed(3)}</span>
+      </div>
+  
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleClaim}
+        disabled={isButtonDisabled}
+        className={`bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold py-3 px-8 rounded-xl shadow-lg ${
+          isButtonDisabled ? "opacity-50 cursor-not-allowed" : "hover:from-blue-600 hover:to-purple-600 transition duration-300 ease-in-out transform hover:scale-105"
+        }`}
       >
-        <div className="flex justify-between items-center">
-          <div>
-            <p className="text-xs text-gray-300 font-bold">Card Name</p>
-            <h2 className="text-xl font-bold text-white">{card.title}</h2>
-          </div>
-          <div className="bg-white/10 p-2 rounded-lg shadow-md">
-            <p className="text-xs font-semibold text-gray-300">{card.rarity}</p>
-          </div>
-        </div>
-
-        <div className="mt-4">
-          <p className="text-gray-300">
-            <strong className="text-4xl text-yellow-400">{card.miningcoins}</strong>
-            <span className="text-xl text-gray-300"> THE</span>
-          </p>
-          <p className="text-sm text-gray-300 mt-1">{card.miningperiod} days</p>
-        </div>
-
-        <div className="my-2">
-          <LinearProgress
-            variant="determinate"
-            value={((card.minedcoins + minedCoins) / card.miningcoins) * 100}
+        {isButtonDisabled ? `Wait ${Math.floor(countdown / 60)}:${String(countdown % 60).padStart(2, '0')}` : "Claim All"}
+      </Button>
+  
+      {userCollection.length === 0 ? (
+        <div className="flex flex-col items-center justify-center w-full h-[200px] bg-gray-700 rounded-2xl shadow-lg p-6 mb-6">
+          <p className="text-xl text-white font-bold mb-4">You don't have any cards yet.</p>
+          <Button
+            variant="contained"
             color="primary"
-            className="rounded-lg"
-          />
+            key={'leaderboard'}
+            onClick={() => console.log(setActiveTab('leaderboard'))}
+            className="shine-effect bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold py-3 px-8 rounded-xl shadow-lg"
+          >
+            Buy your first card
+          </Button>
         </div>
+      ) : (
+        <div className="flex flex-wrap justify-center gap-6">
+        {userCollection.map((card, index) => {
+  const minedCoins = calculateMinedCoins(card, serverTime || new Date());
+  const remainingPercentage = ((card.remainingcoins / card.miningcoins) * 100).toFixed(2);
 
-        <div className="flex justify-end items-center mt-1 space-x-2 whitespace-nowrap">
-          <p className="text-xs text-gray-400">Remaining Coins:</p>
-          <p className="text-xs font-bold text-gray-300">{Math.round(card.remainingcoins)}  ({remainingPercentage}%)</p>
+  const bgColor = (() => {
+    switch (card.rarity) {
+      case "Common":
+        return "bg-[#121212] border-[#8A2BE2]";
+      case "Rare":
+        return "bg-[#1B0036] border-[#00FFFF]";
+      case "Epic":
+        return "bg-[#30003B] border-[#FF00FF]";
+      default:
+        return "bg-[#1A1A1A] border-[#C0C0C0]";
+    }
+  })();
+
+  return (
+    <div
+      key={index}
+      className={`relative w-[300px] h-[200px] ${bgColor} border-2 rounded-2xl shadow-lg p-3 flex flex-col justify-between mb-4`}
+      style={{
+        backgroundImage: 'radial-gradient(circle, rgba(255,255,255,0.1) 0%, transparent 60%)',
+      }}
+    >
+      {/* Верхняя часть карточки */}
+      <div className="flex justify-between items-center mb-1">
+        <div>
+          <p className="text-[10px] text-gray-400 font-bold">Card Name</p>
+          <h2 className="text-sm font-bold text-white truncate">{card.title}</h2>
+        </div>
+        <div className="bg-white/10 p-[2px] rounded-md shadow-md">
+          <p className="text-[10px] font-medium text-gray-300">{card.rarity}</p>
         </div>
       </div>
-    );
-  })
-)}
+
+      {/* Основная информация */}
+      <div>
+        <p className="text-gray-300 text-xs">
+          <strong className="text-xl text-yellow-400">{card.miningcoins}</strong>
+          <span className="text-sm text-gray-300"> THE</span>
+        </p>
+        <p className="text-[10px] text-gray-400 mt-1">{card.miningperiod} days</p>
+      </div>
+
+      {/* Прогресс добычи */}
+      <div className="my-1">
+        <LinearProgress
+          variant="determinate"
+          value={((card.minedcoins + minedCoins) / card.miningcoins) * 100}
+          color="primary"
+          className="rounded-lg"
+        />
+      </div>
+
+      <div className="flex justify-between items-center mt-1 space-x-2 whitespace-nowrap">
+        <p className="text-[10px] text-gray-400">Remaining Coins:</p>
+        <p className="text-[10px] font-bold text-gray-300">{Math.round(card.remainingcoins)}  ({remainingPercentage}%)</p>
+      </div>
+
+      {/* Новый блок внутри карточки */}
+      <div className="flex justify-between items-center bg-[#1e1e1e] rounded-md p-1 mt-2">
+        <div className="flex items-center space-x-1">
+          <span
+            className={`w-2.5 h-2.5 rounded-full ${
+              card.isActive ? "bg-green-500" : "bg-red-500"
+            }`}
+          />
+          <p className="text-[10px] font-medium text-gray-300">
+            {card.isActive ? "Active" : "Inactive"}
+          </p>
+        </div>
+        <div className="text-right">
+          <p className="text-[10px] text-gray-400">Profit per Hour:</p>
+          <p className="text-sm font-bold text-yellow-400">
+            {card.profitperhour.toFixed(2)} THE
+          </p>
+        </div>
       </div>
     </div>
+  );
+})}
+        </div>
+      )}
+    </div>
+  </div>
+  
   );
 };
 
